189c75b0652e7539ab31d4f4654c1f80dd5e2af5
Carl <carlvalero14@gmail.com>
2021-09-30 14:01:22 +0800

    faq section

---
+++ app/src/components/faqSection.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faqsection">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
+            voluptate possimus illo enim minus nisi et consequuntur odit.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
+            voluptate possimus illo enim minus nisi et consequuntur odit.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different payment methods</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
+            voluptate possimus illo enim minus nisi et consequuntur odit.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
+            voluptate possimus illo enim minus nisi et consequuntur odit.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- app/src/pages/aboutUsPage.js
+++ app/src/pages/aboutUsPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutSection from "../components/aboutSection";
 import ServicesSection from "../components/servicesSection";
+import FaqSection from "../components/faqSection";
 
 const AboutUsPage = () => {
   return (
     <div className="aboutUsPage">
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </div>
   );
 };
